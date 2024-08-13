b6694c7864a11dd5863382d868395560b19b0b13
yama-shu <g2254856@cc.kyoto-su.ac.jp>
2024-08-13 15:12:20 +0900

    ホームと通知画面の変更

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Tabs, Tab, TextField, InputAdornment } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Home() {
-  return <div>ホーム画面</div>;
+  const [value, setValue] = React.useState('one');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <div>
+      <Avatar src="/broken-image.jpg" />
+      </div>
+      
+      <div style={{ width: '100%' }}>
+        <Tabs value={value} onChange={handleChange} centered sx={{ backgroundColor: '#f0f0f0', width: '100%' }}>
+          <Tab label="制作物" />
+          <Tab label="仲間募集" />
+          <Tab label="イベント" />
+        </Tabs>
+      </div>
+      <div>
+      <TextField 
+          fullWidth 
+          id="fullWidth"
+          InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+            <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        placeholder="検索"
+      />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
