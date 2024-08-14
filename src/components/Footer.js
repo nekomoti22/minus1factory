4b7f1510f1dd39b4f1b2f27a911f2827fcb9ac05
so-ma0723 <souma-.-yade23@au.com>
2024-08-14 10:54:19 +0900

    Save local changes before merge

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeIcon from '@mui/icons-material/Home';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Footer() {
   return (
@@ -15,14 +13,6 @@ function Footer() {
       <Link to="/create" className="footer-link">
         <AddCircleIcon fontSize="small" />
         <span className="icon-text">Post</span>
-      </Link>
-      <Link to="/notifications" className="footer-link">
-        <NotificationsIcon fontSize="small" />
-        <span className="icon-text">Notice</span>
-      </Link>
-      <Link to="/account" className="footer-link">
-        <AccountCircleIcon fontSize="small" />
-        <span className="icon-text">Account</span>
       </Link>
     </footer>
   );
