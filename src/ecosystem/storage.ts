141bd96a37037b41d85545e763906f387870b1de
yama-shu <g2254856@cc.kyoto-su.ac.jp>
2024-08-16 03:21:14 +0900

    画像と動画の投稿可能化

--- src/ecosystem/storage.ts
+++ src/ecosystem/storage.ts
@@ -22,8 +22,12 @@ export const uploadStorage = async ({
 
   const file = filelist[0]; // 1ファイルアップロード
   console.log("ファイルが選択された");
+  const extension = file.name.split('.').pop();
+  console.log("extention",extension);
+  
+  
 
-  const pathName = `characters/${uuidv4()}`; // パス名の設定
+  const pathName = `characters/${uuidv4()}.${extension}`; // パス名の設定
   console.log("パスの作成ができた");
   const { data, error } = await supabase.storage
     .from(bucketName)
@@ -31,7 +35,7 @@ export const uploadStorage = async ({
       cacheControl: "3600",
       upsert: false,
     });
-  console.log("アップロードができた");
+  console.log("アップロードができた",data?.path);
   if (error) throw error;
   return {
     path: data?.path ?? null,
