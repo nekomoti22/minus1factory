94353d11dadfb2286243aacb5fd76b8bbec582a6
so-ma0723 <souma-.-yade23@au.com>
2024-08-15 12:16:54 +0900

    ログイン認証機能の追加

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,75 @@
-import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
-// import Box from '@mui/material/Box';
-// import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import logoImage from '../image/SNSicon.webp';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import logoImage from "../image/SNSicon.webp";
+import { supabase } from "../lib/supabase-client";
 
 function Login() {
-    console.log('Login');
-    const router = useNavigate();
+  console.log("Login");
+  /**
+   * ボタンクリック時の処理
+   */
+  const handleClick = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `http://localhost:3000/home`,
+        // skipBrowserRedirect: true,
+      },
+    });
 
-    /**
-     * ボタンクリック時の処理
-     */
-    const handleClick = (pathParam) => {
-        router(pathParam);
-      };
-    return (
-    
-      <div className="login" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h2 className="titlename">Product キャッチ</h2>
-         {/* 画像を追加 */}
-        <img src={logoImage} alt="ロゴ" style={{ width: '200px', height: 'auto', margin: '20px 0' }} />
-        
-        <Stack spacing={5} direction="column">
-          <Button variant="contained" size="large" onClick={() => handleClick('Home')} style={{ fontSize: '1.2rem', padding: '15px 30px' }}>GitHubで認証</Button>
-        </Stack>
-        <h2 className="title">Product キャッチはgithubを使い制作物の共有に特化したエンジニア向けSNSです</h2>
-      </div>
-    );
-  }
+    await console.log(supabase.auth.getUser());
+    await console.log(supabase.auth.getSession());
+    await console.log(data, error);
+
+    const user = await supabase.auth.getUser();
+
+    const userName = await user.data.user.identities[0].identity_data.user_name;
+    const userId = user.data.user.id;
+    console.log(userName);
+    console.log(userId);
+    // その変数を使ってデータベースにINSERT
+    const { error: insertError } = await supabase
+      .from("users")
+      .update({ full_name: userName })
+      .eq("id", userId);
+
+    if (insertError) {
+      console.error("Error inserting data:", insertError);
+    } else {
+      console.log("Data inserted successfully");
+    }
+  };
+
+  return (
+    <div
+      className="login"
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1 className="titlename">Product キャッチ</h1>
+      {/* 画像を追加 */}
+      <img
+        src={logoImage}
+        alt="ロゴ"
+        style={{ width: "200px", height: "auto", margin: "20px 0" }}
+      />
+
+      <Stack spacing={5} direction="column">
+        <Button
+          variant="contained"
+          size="large"
+          onClick={handleClick}
+          style={{ fontSize: "1.2rem", padding: "15px 30px" }}
+        >
+          GitHubで認証
+        </Button>
+      </Stack>
+      <h1 className="title">
+        Product
+        キャッチはgithubを使い制作物の共有に特化したエンジニア向けSNSです
+      </h1>
+    </div>
+  );
+}
+
 export default Login;
