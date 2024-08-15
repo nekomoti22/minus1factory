703ba8e99ccaa5941c356d55354496871763c465
nekomoti22 <g2453602@cc.kyoto-su.ac.jp>
2024-08-16 06:03:41 +0900

    chore: デプロイエラーのデバッグ用にlogを追加

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,7 +1,12 @@
-import { createClient } from  "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
+console.log("---------REACT_APP_SUPABASE_URL-----------");
+console.log(process.env.REACT_APP_SUPABASE_URL);
+
+console.log("---------REACT_APP_SUPABASE_ANON_KEY-----------");
+console.log(process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY ||  "";
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "";
 
-export const supabase = createClient (supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
