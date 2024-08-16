9c86e9cad06b3d70bbee60e98946ae1a4fbe2ee8
yama-shu <g2254856@cc.kyoto-su.ac.jp>
2024-08-16 13:49:22 +0900

    fix

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./PostCard.css";
 import ShareButton from "./ShareButton";
 import { IoIosHeartEmpty, IoIosHeart } from "react-icons/io"; // IoIosHeart をインポート
