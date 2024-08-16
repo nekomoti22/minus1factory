04932f7f959987b9ca81cdb4f82d5143f31b8ab0
nekomoti22 <g2453602@cc.kyoto-su.ac.jp>
2024-08-16 10:24:37 +0900

    fix: 環境変数を直打ちしました

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -6,7 +6,7 @@ console.log(process.env.REACT_APP_SUPABASE_URL);
 console.log("---------REACT_APP_SUPABASE_ANON_KEY-----------");
 console.log(process.env.REACT_APP_SUPABASE_ANON_KEY);
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "";
+const supabaseUrl = "https://cdvdeesoyjnugbafkrul.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkdmRlZXNveWpudWdiYWZrcnVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI1NzU0MzEsImV4cCI6MjAzODE1MTQzMX0.jJGXFeqyQyEDSB4uRjqb0TN1UKbhbgnklXZ4ZkNzgXk";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
